846036c07bca3ee85de77b1d32fba1e2121a7322
RealPacket <107498533+RealPacket@users.noreply.github.com>
2024-08-06 17:40:23 -0500

    Fix CI errors
    drops support for browsers that don't have support for ESM or TLA (like safari 11 and chrome 88), I don't care that you still want to use your 69420 year old browser, just upgrade it.
    It's essentially the same thing to me as asking for me to support Firefox 1.
    I can support newer versions, but not the old ones.

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+// https://stackoverflow.com/questions/72618944/get-error-to-build-my-project-in-vite-top-level-await-is-not-available-in-the
+// seriously, screw your IE 11, why would we want to support it?
+// oh, and your chrome 88 or something, don't care.
+// if it's more than 8 or 10 years old, probably shouldn't be supported.
+export default defineConfig({
+	build: {
+		target: "esnext", //browsers can handle the latest ES features
+	},
+});
