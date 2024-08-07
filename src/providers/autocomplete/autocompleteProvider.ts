ad47c9dcb1c82c43bad373dee7189eb182095fc6
RealPacket <107498533+RealPacket@users.noreply.github.com>
2024-08-06 21:41:59 -0500

    add fake autocomplete provider
    It's for ease of development, it isn't used yet but it's there for any time I want to start working on autocomplete, it's really just a template.

---
+++ src/providers/autocomplete/autocompleteProvider.ts
@@ -0,0 +1,12 @@
+import * as monaco from "monaco-editor";
+
+export const autoCompleteProvider: monaco.languages.CompletionItemProvider = {
+	triggerCharacters: ["."],
+	provideCompletionItems(_model, _pos, _ctx, _tok) {
+		// TODO: auto complete not implemented yet.
+		console.log("TODO: provideCompletionItems");
+		return {
+			suggestions: []
+		};
+	}
+};
